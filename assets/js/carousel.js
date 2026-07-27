1f7ce4df5119fcbff9391a5d0aceee25ce523de5
Kenny <kablesgaming@gmail.com>
2026-07-27 17:34:02 -0400

    Initial commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,63 @@
+import { hexToString, removeColorClasses } from "./colors.js";
+
+const carouselSection = document.querySelector("#carousel");
+
+const titleEl = carouselSection.querySelector(".carousel__title");
+const cardEl = carouselSection.querySelector(".carousel__card");
+const cardTextEl = carouselSection.querySelector(".carousel__card-text");
+
+const leftBtn = carouselSection.querySelector(".carousel__btn_type_left");
+const rightBtn = carouselSection.querySelector(".carousel__btn_type_right");
+const flipBtn = carouselSection.querySelector(".carousel__btn_type_flip");
+
+function renderCarouselView(deck) {
+  let currentIndex = 0;
+  let showingQuestion = true;
+
+  function updateDisplay() {
+    const currentCard = deck.cards[currentIndex];
+
+    titleEl.textContent = `${currentIndex + 1} of ${deck.cards.length} · ${deck.name}`;
+
+    removeColorClasses(cardEl);
+
+    const colorName = hexToString(deck.color);
+    cardEl.classList.add(`carousel__card_color_${colorName}`);
+
+    if (showingQuestion) {
+      cardTextEl.textContent = currentCard.question;
+    } else {
+      cardTextEl.textContent = currentCard.answer;
+
+      removeColorClasses(cardEl);
+      cardEl.classList.add("carousel__card_color_white");
+    }
+    leftBtn.disabled = currentIndex === 0;
+    rightBtn.disabled = currentIndex === deck.cards.length - 1;
+  }
+
+  flipBtn.addEventListener("click", () => {
+    showingQuestion = !showingQuestion;
+    updateDisplay();
+  });
+
+  leftBtn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      showingQuestion = true;
+      updateDisplay();
+    }
+  });
+
+  rightBtn.addEventListener("click", () => {
+    if (currentIndex < deck.cards.length - 1) {
+      currentIndex++;
+      showingQuestion = true;
+      updateDisplay();
+    }
+  });
+
+  updateDisplay();
+}
+
+export { renderCarouselView };
