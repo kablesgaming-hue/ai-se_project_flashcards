3cf7cbcf4926307d89625a9bce12558ba93cf7e5
Kenny <kablesgaming@gmail.com>
2026-08-05 12:55:09 -0400

    Add new deck view and enable submit button

--- assets/js/new-deck-view.js
+++ assets/js/new-deck-view.js
@@ -1,7 +1,5 @@
 const formEl = document.querySelector("#new-deck-form");
-
 const submitBtn = formEl.querySelector(".new-deck-view__submit-btn");
-
 const textareaEl = formEl.querySelector(".new-deck-view__textarea");
 
 function disableSubmitBtn() {
