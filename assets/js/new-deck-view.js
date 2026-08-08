ff84814fcaee3f091e7a07380cfea26a584709bf
Kenny <kablesgaming@gmail.com>
2026-08-08 17:20:42 -0400

    Implement error modal and form validation

--- assets/js/new-deck-view.js
+++ assets/js/new-deck-view.js
@@ -6,6 +6,10 @@ const formEl = document.querySelector("#new-deck-form");
 const submitBtn = formEl.querySelector(".new-deck-view__submit-btn");
 const textareaEl = formEl.querySelector(".new-deck-view__textarea");
 
+const errorModal = document.querySelector("#error-modal");
+const errorCloseBtn = errorModal.querySelector(".modal__close");
+const errorMessageEl = errorModal.querySelector(".modal__error");
+
 function slugify(str) {
   return str
     .toLowerCase()
@@ -28,8 +32,33 @@ function normalizeColor(color) {
   return `#${hex.toLowerCase()}`;
 }
 
+function closeModal(modal) {
+  modal.classList.remove("modal_visible");
+}
+
 function disableSubmitBtn() {
   submitBtn.disabled = false;
+}
+
+function showError(message) {
+  errorMessageEl.textContent = message;
+  errorModal.classList.add("modal_visible");
+}
+
+function validateName(name) {
+  if (typeof name != "string" || name.length < 2 || name.length > 80) {
+    return null;
+  }
+
+  return name;
+}
+
+function parseJSON(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    return null;
+  }
 }
 
 function handleNewDeckSubmit(e) {
@@ -38,11 +67,38 @@ function handleNewDeckSubmit(e) {
   const formData = new FormData(e.target);
   const values = Object.fromEntries(formData);
 
-  const jsonData = JSON.parse(values["deck-json"]);
+  const jsonData = parseJSON(values["deck-json"]);
+
+  if (jsonData === null) {
+    showError("JSON parsing failed");
+    return;
+  }
+
+  if (validateName(jsonData.name) === null) {
+    showError("Name must be a string between 2 and 80 characters");
+    return;
+  }
+
+  if (!Array.isArray(jsonData.cards)) {
+    showError("Cards must be an array");
+    return;
+  }
+
+  const colorValue = normalizeColor(values.color);
+
+  if (
+    typeof jsonData.color === "string" &&
+    jsonData.color.toLowerCase() !== colorValue
+  ) {
+    showError(
+      `The JSON color ${jsonData.color} does not match the selected color ${colorValue}`,
+    );
+    return;
+  }
 
   const deck = {
     id: `${slugify(jsonData.name)}-${Date.now()}`,
-    color: normalizeColor(values.color),
+    color: colorValue,
     name: jsonData.name,
     cards: jsonData.cards,
   };
@@ -53,5 +109,9 @@ function handleNewDeckSubmit(e) {
 }
 
 formEl.addEventListener("submit", handleNewDeckSubmit);
+
+errorCloseBtn.addEventListener("click", () => {
+  closeModal(errorModal);
+});
 
 export { disableSubmitBtn };
